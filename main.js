c75714d86be5bcd12d8d6b46342e0fcfab4e5e89
Henrique <Henriqueb96@outlook.com>
2023-10-18 00:19:12 -0300

    comit inicial

---
+++ main.js
@@ -0,0 +1,29 @@
+$('header button').click(function() {
+    $('form').slideDown();
+
+    $('#botao-cancelar').click(function(){
+        $('form').slideUp();
+    })
+})
+
+$('form').on('submit', function(e) {
+    e.preventDefault();
+})
+
+$('form').on('submit', function(e) {
+    e.preventDefault();
+    const enderecoDaNovaImagem = $('#endereco-img-nova').val();
+    const novoItem = $('<li style="display: none"></li>');
+
+    $(`<img src="${enderecoDaNovaImagem}" />`).appendTo(novoItem);
+    $(`
+        <div class="overlay-imagem-link">
+            <a href= "${enderecoDaNovaImagem}" target ="_blank" tittle ="ver imagem em tamanho real">
+                Ver imagem em tamanho real
+            </a>
+        </div>
+    `).appendTo(novoItem);
+    $(novoItem).appendTo('ul');
+    $(novoItem).fadeIn(3000);
+    $('#endereco-img-nova').val('')
+})
